e4bd0cfabd2b95fef3582b181dc96be4e2e15dec
speedskater1610 <speedskater1610@gmail.com>
2026-01-22 11:31:50 -0500

    Update odom.hpp: add inertial

--- include/odom.hpp
+++ include/odom.hpp
@@ -1,3 +1,14 @@
+#ifndef ODOM_HPP
+#define ODOM_HPP
+#pragma once
+
+#include <cstdint>
+
+extern pros::Imu inertial;
+
 extern pros::Distance distanceFront;
 extern pros::Distance distanceLeft;
 extern pros::Distance distanceRight;
+
+
+#endif // ODOM_HPP
